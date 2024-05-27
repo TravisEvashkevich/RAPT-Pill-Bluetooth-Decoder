a8f847dddc8d1faa2552b442678ad19152249836
Travis Evashkevich <smackmeawake@gmail.com>
2024-05-27 10:52:20 +0100

    Add data.json reading and gravity db check
    - Data.json added as way to define database login and pill(s) setup without having to open the python file.
    
    - expose properties for data going to DB
    
    - added in DB check in case starting gravity is set to non 0 value in data.json - we will check first if the DB has a first value from the last 10 days and use that if it exists else whatever the user put in the data.json
    
    - Added timstamp to data blob in case we need it to make easier graphs
    
    - Comments/doc strings

--- src/RaptPill_Decoder.py
+++ src/RaptPill_Decoder.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
 import asyncio
-import sys
-from time import sleep
+from pathlib import Path
+import json
 from typing import Optional
 from bleak import BleakScanner
 from bleak.backends.device import BLEDevice
 from bleak.backends.scanner import AdvertisementData
-from binascii import hexlify
 from struct import unpack
 from collections import namedtuple
 from datetime import datetime
+import traceback
+
+from influxdb.resultset import ResultSet
 
 # Taken from rapt_ble on github (https://github.com/sairon/rapt-ble/blob/main/src/rapt_ble/parser.py#L14) as well as the decode_rapt_data
 RAPTPillMetricsV1 = namedtuple(
@@ -18,6 +20,7 @@ RAPTPillMetricsV1 = namedtuple(
 RAPTPillMetricsV2 = namedtuple(
     "RAPTPillMetrics", "hasGravityVel, gravityVel, temperature, gravity, x, y, z, battery"
 )
+
 class InfluxDbWrapper(object):
     def __init__(self, db_name:str, db_host:str, db_port:int, db_username:str, db_pwd:str):
         self.db_name = db_name
@@ -27,11 +30,18 @@ class InfluxDbWrapper(object):
         self.db_pwd = db_pwd
         try:
             from influxdb import InfluxDBClient
-            self.client = InfluxDBClient(host=self.db_host, port=self.db_port, username=self._username, password=self.db_pwd, database=self.db_name)
+            self.client = InfluxDBClient(host=self.db_host, port=self.db_port, username=self.db_username, password=self.db_pwd, database=self.db_name)
 
         except:
+            print(traceback.format_exc())
             self.client = None
+    
     def add_datapoint(self, pill:RaptPill):
+        """Add a datapoint to the database if connected
+
+        Args:
+            pill (RaptPill): pill data to add
+        """        
         if not self.client:
             print("!!!! Couldn't connect to DB !!!!")
             return
@@ -43,31 +53,48 @@ class InfluxDbWrapper(object):
                     "version": pill.version
                 },
                 "fields": {
-                    "gravity_velocity": pill.__gravity_velocity,
-                    "curr_gravity": pill.__curr_gravity,
-                    "abv": pill.__abv,
-                    "temperature": pill.__temperature,
-                    "battery": pill.__battery,
-                    "x": pill.__x,
-                    "y": pill.__y,
-                    "z": pill.__z
-                },
-                # "time": datetime.now(datetime="UTC").strftime('%Y-%m-%dT%H:%M:%SZ')
+                    "sessionName":pill.session_name,
+                    "gravity_velocity": pill.gravity_velocity,
+                    "curr_gravity": pill.curr_gravity,
+                    "abv": pill.abv,
+                    "temperature": pill.temperature,
+                    "battery": pill.battery,
+                    "x": pill.x_accel,
+                    "y": pill.y_accel,
+                    "z": pill.z_accel,
+                    "timestamp": pill.last_event
+                }
             }
         ]
 
         # Write the data to the database
         self.client.write_points(inf_data)
 
+    def run_query(self, query:str) -> None | asyncio.Generator[ResultSet, asyncio.Any, None] | ResultSet | list[ResultSet]:
+        """Run a query on the database and get the results
+
+        Args:
+            query (str): query to run
+
+        Returns:
+            None | asyncio.Generator[ResultSet, asyncio.Any, None] | ResultSet | list[ResultSet]: results or None
+        """        
+        if not self.client:
+            print("!!!! Couldn't connect to DB !!!!")
+            return 
+
+        return self.client.query(query)
+
 class RaptPill(object):
     active_pollers = []
-    def __init__(self, session_name:str, mac_address:str, poll_interval:int, influx_database_details:Optional[InfluxDbWrapper]=None, temp_as_celsius:bool=True):
+    def __init__(self, session_name:str, mac_address:str, poll_interval:int, influx_database_details:Optional[InfluxDbWrapper]=None, temp_as_celsius:bool=True ):
         """Create a Pill object to actively poll for data
 
         Args:
             session_name (str): name of the session we are tracking
             mac_address (str): address of the pill we are tracking so we know which bluetooth device to watch for
             poll_interval (int): how often should we poll for data in seconds. This ideally will be slightly longer than what is set in the Pill firmware
+            influx_database_details(InfluxDbWrapper): details for database to log data to - If None, no data is logged and is just printed to output.
             temp_as_celsius(bool): set False if you want temp as F instead
         """
         self.__db_details = influx_database_details
@@ -132,6 +159,46 @@ class RaptPill(object):
     @property
     def session_name(self)->str:
         return self.__session_name
+
+    @property
+    def gravity_velocity(self) ->float:
+        return self.__gravity_velocity
+    
+    @property
+    def curr_gravity(self):
+        return self.__curr_gravity
+    
+    @property
+    def abv(self):
+        return self.__abv
+    
+    @property
+    def temperature(self):
+        return self.__temperature
+    
+    @property
+    def battery(self):
+        return self.__battery
+
+    @property
+    def version(self):
+        return self.__api_version
+
+    @property
+    def x_accel(self):
+        return self.__x
+    
+    @property
+    def y_accel(self):
+        return self.__y
+    
+    @property
+    def z_accel(self):
+        return self.__z
+
+    @property
+    def last_event(self):
+        return self.__last_event
 
     def start_session(self):
         print(f"Starting Session: {self.session_name}")
@@ -212,7 +279,7 @@ class RaptPill(object):
         if len(data) != 23:
             raise ValueError("advertisment data must have length 23")
         
-        print(f"===> raw_data: {data}")
+        # print(f"===> raw_data: {data}")
 
         # Extract and check the version
         prefix, version = unpack(">2sB", data[:3])
@@ -231,7 +298,7 @@ class RaptPill(object):
         # print("date and time =", dt_string)
         if not self.__starting_gravity_set:
             self.starting_gravity = round(metrics_raw.gravity / 1000, 4)
-        self.version = version
+        self.__api_version = version
         self.__gravity_velocity = metrics_raw.gravityVel
         self.__curr_gravity = round(metrics_raw.gravity / 1000, 4)
         self.__abv = self.calculate_abv(self.__curr_gravity)
@@ -241,7 +308,13 @@ class RaptPill(object):
         self.__x = metrics_raw.x / 16
         self.__y = metrics_raw.y / 16
         self.__z = metrics_raw.z / 16
+
+        if self.__log_to_db:
+            self.__db_details.add_datapoint(self)
+        else:
+            print(self)
     
+
     def __repr__(self):
         return("Current Data: \n"
             f"Session Name: {self.__session_name} , " "\n"
@@ -259,11 +332,58 @@ class RaptPill(object):
                )
 
 
-async def main():
-    influx_details = InfluxDbWrapper("RaptPill", "localhost", 8086, "travis", "notSafePassword")
-    # MAC addresses of your RAPT Pill(s) - in case you have more (This hasn't been actually tested but it should in theory work.)
-    pill = RaptPill("Hopped Mead", "78:E3:6D:29:19:16", 120, influx_database_details=influx_details)
-    pill.start_session()
+async def main() -> None:
+    # Handle setup of database and pill(s) 
+    data_path = Path(__file__).parent.joinpath("data.json")
+    
+    # if data is filled in data.json file use it and start sessions and database (if set)
+    if data_path.exists():
+        # Read data.json and spin up processes
+        data = json.loads(data_path.read_text())
+        influx_details = None
+        if data.get("InfluxDb Details", None):
+            db_details = data.get("InfluxDb Details", None)
+            influx_details = InfluxDbWrapper(db_details.get("Database Name", None), 
+                                             db_details.get("Database Address", ""), 
+                                             db_details.get("Database Port", 8086), 
+                                             db_details.get("Database Username", ""), 
+                                             db_details.get("Database Password", ""), 
+                                             )
+        for pill_details in data.get("Sessions", []):
+            # MAC addresses of your RAPT Pill(s) - in case you have more (This hasn't been actually tested but it should in theory work.)
+            pill = RaptPill(pill_details.get("Session Name", "NoSessionNameSet"), 
+                            pill_details.get("Mac Address",None),
+                            pill_details.get("Poll Interval",""),
+                            influx_database_details=influx_details,
+                            temp_as_celsius=pill_details.get("Temp in C",True))
+            
+            # We set the Starting Gravity here if we had a disconnect or something like that and we need to set it again (and we don't want the abv to be wrong)
+            # In general this shouldn't be set - only if a session has been lost and needs to be restarted mid session
+            # TODO: maybe check the database for the first gravity of this session instead?
+            if pill_details.get("Starting Gravity", 0) != 0:
+                # Check if this session is in the database already over the the last 10 days and check the first gravity we logged if it is
+                query = f'''
+                SELECT "curr_gravity"
+                FROM "rapt_pill_metrics"
+                WHERE "sessionName" = '{pill_details.get('Session Name', '')}' AND time > now() - 10d
+                ORDER BY time ASC
+                LIMIT 1
+                '''
+                results = influx_details.run_query(query)
+                points = list(results.get_points())
+                starting_gravity_in_db = points[0].get("curr_gravity", 0)
+                if starting_gravity_in_db != 0:
+                    pill.starting_gravity = starting_gravity_in_db
+                else:                    
+                    pill.starting_gravity = pill_details.get("Starting Gravity", 0)
+
+            pill.start_session()
+    else:
+        # fill in all the details yourself here if you don't want to use the data.json
+        influx_details = InfluxDbWrapper("Database Name", "localhost", 8086, "user", "somePassword")
+        # MAC addresses of your RAPT Pill(s) - in case you have more (This hasn't been actually tested but it should in theory work.)
+        pill = RaptPill("Exmple Session Name", "78:E3:6D:29:19:16", 120, influx_database_details=influx_details)
+        pill.start_session()
 
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
